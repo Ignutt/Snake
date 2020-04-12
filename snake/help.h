4c470df68c9bd1e1f3a281e13892347b4722e58e
Pibody <mister.strays@gmail.com>
2020-04-12 18:22:58 +0300

    Создание управление(недоработано до идиала).\nСоздание класса Apple.\nВозможность собирать яблоко и оно будет менять свою позицию на поле.\n

---
+++ snake/help.h
@@ -0,0 +1,152 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <vector>
+
+#include <ctime>
+
+using namespace std;
+using namespace sf;
+
+class Cell {
+public:
+	RectangleShape main, border;
+	float size = 25;
+	Vector2f direct;
+
+	Cell(float _size, float proc) {
+		size = _size;
+
+		border.setSize(Vector2f(_size, _size));
+		Color border_color(7, 2, 171);
+		border.setFillColor(border_color);
+
+		float size_main = _size * proc;
+		main.setSize(Vector2f(size_main, size_main));
+		Color main_color(94, 89, 255);
+		main.setFillColor(main_color);
+		main.setPosition(Vector2f((_size - size_main) / 2, (_size - size_main) / 2));
+	}
+
+	void DrawCell(RenderWindow& win) {
+		win.draw(border);
+		win.draw(main);
+	}
+
+	void TranslateCell(Vector2f v2) {
+		border.move(v2);
+		main.move(v2);
+	}
+
+	//RectangleShape returnMain() { return main; }
+	//RectangleShape returnBorder() { return border; }
+};
+
+class Apple {
+public:
+
+	Vector2f pos;
+	RectangleShape main, border;
+	float size = 25, proc;
+
+	Apple(float _size, float _proc) {
+		size = _size;
+		proc = _proc;
+
+		border.setSize(Vector2f(_size, _size));
+		Color border_color(255, 0, 0);
+		border.setFillColor(border_color);
+
+		float size_main = _size * _proc;
+		main.setSize(Vector2f(size_main, size_main));
+		Color main_color(145, 168, 10);
+		main.setFillColor(main_color);
+		main.setPosition(Vector2f((_size - size_main) / 2, (_size - size_main) / 2));
+		Spawn();
+	}
+
+	void SetPosMainPart() { // вынес чтобы не повторять две строчки кода постоянно...
+		float size_main = size * proc;
+		main.setPosition(Vector2f((size - size_main) / 2, (size - size_main) / 2));
+	}
+
+	void Spawn() {
+		srand(time(0));
+		int x = (rand() % 19 + 2) * 25, y = (rand() % 19 + 2) * 25;
+		cout << "Respawn! New points are: x = " << x << " y = " << y << endl;
+		Vector2f _pos = Vector2f(x, y);
+		pos = _pos;
+		SetPosMainPart(); // для того чтобы mainPart встала по центру
+		main.move(_pos);
+		border.setPosition(_pos);
+
+	}
+
+	void DrawApple(RenderWindow& win) {
+		win.draw(border);
+		win.draw(main);
+	}
+};
+
+class Snake {
+public:
+	vector<Cell> cells;
+
+	void TranslateSnake(Vector2f v2) {
+		for (int i = 0; i < cells.size(); i++)
+			cells[i].TranslateCell(v2);
+	}
+
+	void TranslateSnake() {
+		for (int i = 0; i < cells.size(); i++)
+			cells[i].TranslateCell(cells[i].direct);
+	}
+
+	Snake(int size, Cell cell) {
+		for (int i = 0; i < size; i++) {
+			cells.push_back(cell);
+			TranslateSnake(Vector2f(0, cell.size));
+			cells[i].direct = Vector2f(0, 25);
+		}
+		TranslateSnake(Vector2f(0, -cell.size));
+	}
+
+	void Draw(RenderWindow& win) {
+		for (int i = 0; i < cells.size(); i++) {
+			cells[i].DrawCell(win);
+		}
+	}
+
+	void ChangeDirect() {
+		for (int i = cells.size() - 1; i > 0; i--) {
+			if (cells[i].direct != cells[i - 1].direct) {
+				cells[i].direct = cells[i - 1].direct;
+			}
+		}
+	}
+	void CheckCollectApple(Apple &apple) {
+		if (apple.pos == cells[0].border.getPosition()) {
+			apple.Spawn();
+			cout << "Apple was collected!";
+		}
+	}
+};
+
+class Timer {
+public:
+	Time time;
+	Clock clock;
+	float seconds = 0;
+
+	void Update() {
+		time = clock.restart();
+		seconds += time.asSeconds();
+	}
+
+	bool Check(float per) {
+		if (seconds >= per) {
+			seconds = 0;
+			return true;
+		}
+		else return false;
+	}
+};
---
+++ snake/main.cpp
@@ -0,0 +1,54 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>	
+#include "help.h"
+
+using namespace sf;
+using namespace std;
+
+int main()
+{
+
+	Apple apple(25, 0.65f);
+
+	Timer timer;
+	RenderWindow window(VideoMode(500, 500), "Snake");
+	Cell cell(25, 0.65f);
+	Snake snake(8, cell);
+
+	while (window.isOpen())
+	{
+		Event e;
+		timer.Update();
+
+		while (window.pollEvent(e))
+		{
+			if (e.type == Event::Closed)
+				window.close();
+			if (Keyboard::isKeyPressed(Keyboard::Right) && snake.cells[0].direct.x != -25) {
+				snake.cells[0].direct = Vector2f(25, 0);
+				//cout << snake.cells[0].direct.x << " " <<snake.cells[0].direct.y << endl;
+			} else if (Keyboard::isKeyPressed(Keyboard::Left) && snake.cells[0].direct.x != 25) {
+				snake.cells[0].direct = Vector2f(-25, 0);
+				//cout << snake.cells[0].direct.x << " " << snake.cells[0].direct.y << endl;
+			} else if (Keyboard::isKeyPressed(Keyboard::Down) && snake.cells[0].direct.y != -25) {
+				snake.cells[0].direct = Vector2f(0, 25);
+				//cout << snake.cells[0].direct.x << " " << snake.cells[0].direct.y << endl;
+			} else if (Keyboard::isKeyPressed(Keyboard::Up) && snake.cells[0].direct.y != 25) {
+				snake.cells[0].direct = Vector2f(0, -25);
+				//cout << snake.cells[0].direct.x << " " << snake.cells[0].direct.y << endl;
+			}
+
+		}
+		window.clear(Color::Green);
+		snake.Draw(window);
+		apple.DrawApple(window);
+		if (timer.Check(0.5f)) { 
+			cout << "Apple position is: "<< apple.pos.x << " " << apple.pos.y << endl;
+			snake.TranslateSnake(); 
+			snake.ChangeDirect();
+			snake.CheckCollectApple(apple);
+		}
+		window.display();
+	}
+	return 0;
+}
